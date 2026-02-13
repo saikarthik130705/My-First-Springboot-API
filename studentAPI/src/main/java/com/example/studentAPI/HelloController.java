f25f076ead932b8025378a0e9446a403b1316f3f
saikarthik130705 <saikarthik130705@gmail.com>
2026-02-13 17:38:02 +0530

    Initial commit - first springboot API with /hello endpoint

---
+++ studentAPI/src/main/java/com/example/studentAPI/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.studentAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello Sai, your first API is working!";
+    }
+}
