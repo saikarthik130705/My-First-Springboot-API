224755b8b0808ae74b4946ceb2f33b3d27c36d2e
saikarthik130705 <saikarthik130705@gmail.com>
2026-02-13 18:32:09 +0530

    Modified /student endpoint to return multiple students (JSON array)

--- studentAPI/src/main/java/com/example/studentAPI/HelloController.java
+++ studentAPI/src/main/java/com/example/studentAPI/HelloController.java
@@ -3,11 +3,17 @@ package com.example.studentAPI;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/student")
-    public Student getStudent() {
-        return new Student(1, "Sai Karthik", 20);
+    public List<Student> getStudent() {
+        Student s1 = new Student(1, "Sai Karthik", 20);
+        Student s2 = new Student(2, "Sai Dharshan", 19);
+        Student s3 = new Student(3, "Yashwanthi", 45);
+
+        return Arrays.asList(s1, s2, s3);
     }
 }
