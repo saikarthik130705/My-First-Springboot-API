375de1ede7d1574a97fa9444e30b3617a3f36f20
saikarthik130705 <saikarthik130705@gmail.com>
2026-02-15 16:10:14 +0530

    Added POST to /students endpoint and an in-memory students list

--- studentAPI/src/main/java/com/example/studentAPI/HelloController.java
+++ studentAPI/src/main/java/com/example/studentAPI/HelloController.java
@@ -1,19 +1,28 @@
 package com.example.studentAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.*;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/student")
-    public List<Student> getStudent() {
-        Student s1 = new Student(1, "Sai Karthik", 20);
-        Student s2 = new Student(2, "Sai Dharshan", 19);
-        Student s3 = new Student(3, "Yashwanthi", 45);
+    private List<Student> students = new ArrayList<>();
 
-        return Arrays.asList(s1, s2, s3);
+    @GetMapping("/students")
+    public List<Student> getStudent() {
+        
+        return students;
     }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+
+    
 }
